8d9df68e9e2b8d429dbe979d8bd42c3f8d49cfd7
Haroun Louati <57402342+haroun97@users.noreply.github.com>
2025-01-20 19:54:57 +0100

    Time: 4 ms (4.74%), Space: 16.9 MB (90.61%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        # Pointers for nums1, nums2, and the end of merged array in nums1
+        p1, p2, p = m - 1, n - 1, m + n - 1
+
+        # Merge in reverse order
+        while p1 >= 0 and p2 >= 0:
+            print("nums1[p1]", nums1[p1])
+            print("nums2[p2]", nums2[p2])
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+
+        # If there are elements left in nums2, copy them
+        nums1[:p2 + 1] = nums2[:p2 + 1]
+
+
+            
+
+solution = Solution()
+nums1 = []
+nums2 = [1]
+solution.merge(nums1, m=0, nums2=nums2, n=1)
+print(nums1)  # Should print [1, 2, 3, 4, 5]
